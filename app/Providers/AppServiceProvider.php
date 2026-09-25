6ebadbc62bb84d7e8319ad772c9563521545dd3e
Adhi Ardiansyah <adhiardiansyah@users.noreply.github.com>
2026-09-25 15:06:39 +0700

    fix: perbaiki namespace facade URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facade\URL;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
